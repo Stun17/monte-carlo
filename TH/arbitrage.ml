9ff58ae5fa3037bd3fc5d41d9b6feb7506431422
Your Name <you@example.com>
2018-12-10 18:36:17 +0000

    good good good

--- TH/arbitrage.ml
+++ TH/arbitrage.ml
@@ -5,30 +5,32 @@ open Decisions ;;
 open Batteries ;;
 open Printf ;;
 
-let convert (r, s) =
-  let suit = match s with | 0 -> "s" | 1 -> "c" | 2 -> "d" | _ -> "h"
+let myConvi (r, s) =
+  let suit = match s with | 0 -> "♠" | 1 -> "♣" | 2 -> "♦" | _ -> "♥"
   and rank = match r with | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 -> string_of_int (r + 2)
                           | 8 -> "T" | 9 -> "J" | 10 -> "Q" | 11 -> "K" | _ -> "A"
   in rank ^ suit ;;
 
-let mprn rez name =
-  printf "%-10s" name ; List.iter (fun z -> printf "%s " (convert z)) rez ; printf "\n" ;;
+let myPrn rez name =
+  printf "%-10s" name ; List.iter (fun z -> printf "%s " (myConvi z)) rez ; printf "\n" ;;
+
+let mySort = fun (r1, s1) (r2, s2) -> if r1 < r2 then 1 else -1
   
 let prepare cs =
-     let pl1 = List.drop  5 cs |> List.take 2
-     and pl2 = List.drop  7 cs |> List.take 2
-     and pl3 = List.drop  9 cs |> List.take 2
-     and pl4 = List.drop 11 cs |> List.take 2
-     and pl5 = List.drop 13 cs |> List.take 2
-     and pl6 = List.drop 15 cs |> List.take 2
-     and pl7 = List.drop 17 cs |> List.take 2
-     and pl8 = List.drop 19 cs |> List.take 2
-     and pl9 = List.drop 21 cs |> List.take 2
-     and plA = List.drop 23 cs |> List.take 2
+     let pl1 = List.drop  5 cs |> List.take 2 |> List.sort mySort 
+     and pl2 = List.drop  7 cs |> List.take 2 |> List.sort mySort 
+     and pl3 = List.drop  9 cs |> List.take 2 |> List.sort mySort 
+     and pl4 = List.drop 11 cs |> List.take 2 |> List.sort mySort 
+     and pl5 = List.drop 13 cs |> List.take 2 |> List.sort mySort 
+     and pl6 = List.drop 15 cs |> List.take 2 |> List.sort mySort 
+     and pl7 = List.drop 17 cs |> List.take 2 |> List.sort mySort 
+     and pl8 = List.drop 19 cs |> List.take 2 |> List.sort mySort 
+     and pl9 = List.drop 21 cs |> List.take 2 |> List.sort mySort 
+     and plA = List.drop 23 cs |> List.take 2 |> List.sort mySort 
      in ([pl1; pl2; pl3; pl4; pl5; pl6; pl7; pl8; pl9; plA], List.take 5 cs)
 ;;
 
-let mfind predicat cs =
+let myFind predicat cs =
   try
     List.map predicat cs |>
     List.findi (fun i x -> x == true) |>
@@ -36,10 +38,10 @@ let mfind predicat cs =
   with _ -> (12, [])
           
 let rec myWorkFun cs predicat title nextFun =
-  let (k, rez1) = mfind predicat cs
-  in if 0 < List.length rez1 && k < 10
-     then (mprn rez1 title ; myWorkFun (List.drop (k + 1) cs) predicat title nextFun)
-     else nextFun cs  
+  let (k, rez1) = myFind predicat cs
+  in if 0 < List.length rez1
+     then (myPrn rez1 title ; myWorkFun (List.drop (k + 1) cs) predicat title nextFun)
+     else if (10 == List.length cs) then nextFun cs else print_newline () 
 ;;
 
 let isSomeHaveHight = fun cs -> myWorkFun cs isHight    "high"  (fun cs -> ())  ;;
