dc1081966fa6ba7c884e37abc62aa68433c72114
Your Name <you@example.com>
2018-12-14 15:02:10 +0000

    ok done with dupal range

--- TH/rangir.ml
+++ TH/rangir.ml
@@ -12,15 +12,14 @@ module Rangir =
             if r1 == t1 && r2 < t2 then -1 else 0
     ;;
                   
-    let cRank = fun (r1,s1) (r2,s2) -> if r1 > r2 then 1 else -1 ;;
+    let cRank = fun (r1,s1) (r2,s2) -> if r1 >= r2 then 1 else -1 ;;
                
     let pRank = fun xs ->
       match xs with
-      | [(r1,_); (r2,_)] -> if r1 >= r2 then xs else List.rev xs 
+      | [(r1,_); (r2,_)] -> if r1 > r2 then xs else List.rev xs 
       | _ -> xs
     ;;
 
-           
     let rangeHigh = fun css ->
       let pockets = List.map (List.take 2) css |> List.map pRank |> List.sort compare |> List.rev
       in let maxRank = List.max pockets |> List.hd |> fst
@@ -47,27 +46,26 @@ module Rangir =
               List.filter (fun x -> x > -1) |> List.map (List.nth css) |> List.map (List.take 2)
 
     let rangeDupal = fun css ->
-      let ranks = List.map (fun cs ->
-                    List.fold_left
-                      (fun (ra, st) (ru,_) ->
-                        if ra = ru then (ru, ru :: st) else (ru, st)
-                      ) (-1, []) cs
-         ) (List.map (List.sort cRank) css) |> List.map snd |>
-                    List.map (fun xs -> List.sort compare xs |> List.rev)
-     in let maxVV = List.concat ranks |> List.max
-        in let indecies = List.mapi (fun i xs ->
-                              if 2 == List.length xs && maxVV == List.hd xs then i else -1) ranks
-           in List.filter (fun x -> x > -1) indecies |> List.map (List.nth css) |>
-                List.map (List.take 2) |> List.sort dRank |> List.rev |> List.hd
+      let ranks = List.map (fun cs -> 
+                      List.fold_left 
+                        (fun (ra, st) (ru,_) -> 
+                          if ra == ru then (ru, ru :: st) else (ru, st) 
+                        ) (-1, []) cs) (List.map (List.sort cRank) css) |>
+                    List.map snd |> List.map (fun xs -> (List.sort compare xs |> List.rev))
+      in let maxVV = List.concat ranks |> List.max 
+         in List.mapi (fun i xs ->
+                if 2 == List.length xs && maxVV == List.hd xs then i else -1) ranks |>
+              List.filter (fun x -> x > -1) |> List.map (List.nth css) |> 
+              List.map (List.take 2) |> List.sort dRank |> List.rev |> List.hd 
       
   end ;;
 
 
   (* test suite *)
-let board = [(3,1);(7,3);(0,1);(5,1);(2,1)] ;;
+let board = [(3,1);(6,3);(4,1);(11,3);(3,2)] ;;
 let tu2 = Rangir.rangeDupal
             [
-              [( 4, 1); ( 1, 1)] @ board ;
-              [( 7, 2); ( 5, 2)] @ board ;
-              [( 7, 2); ( 5, 2)] @ board ;
+              [(11, 1); ( 1, 1)] @ board ;
+              [(10, 2); (10, 1)] @ board ;
+              [( 7, 2); ( 7, 1)] @ board ;
             ]
