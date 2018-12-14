567ce27a5dcc2b02901ad718653e24b33962ac74
Your Name <you@example.com>
2018-12-14 13:26:34 +0000

    start working with rangeDupal

--- TH/rangir.ml
+++ TH/rangir.ml
@@ -12,14 +12,14 @@ module Rangir =
       match xs with
       | [(r1,_); (r2,_)] -> if r1 >= r2 then xs else List.rev xs 
       | _ -> xs
+    ;;
 
            
     let rangeHigh =
       fun css ->
-      let rez1 = List.map (fun cs -> List.take 2 cs) css |>
-                   List.map pRank |> List.sort compare |> List.rev
-      in let maxRank = List.max rez1 |> List.hd |> fst
-         in let rez2 = List.take_while (fun [(r1, _) ; _] -> r1 == maxRank) rez1
+      let pockets = List.map (List.take 2) css |> List.map pRank |> List.sort compare |> List.rev
+      in let maxRank = List.max pockets |> List.hd |> fst
+         in let goodPockets = List.take_while (fun [(r1, _) ; _] -> r1 == maxRank) pockets
             in List.fold_left ( fun (v, st) xs ->
                                 match xs with
                                 | [_ ; (r2, _)] ->
@@ -27,32 +27,41 @@ module Rangir =
                                      if r2 == v then (v, xs :: st) else (v, st)
                                 | _ -> (v, st)
                               )
-                              (0, [])
-                              rez2
-               |> snd 
+                              (0, []) goodPockets |> snd
+    ;;
             
     let rangePairs =
       fun css ->
       let ranks = List.map (fun cs ->
-                    List.fold_left (fun (ra, st) (ru, _) ->
-                        if ra == ru
-                        then (ru, ru)
-                        else (ru, st)
-                      )
-                                   (-1, -1) cs)
-                         (List.map (List.sort cRank) css) |> List.map snd
+                      List.fold_left
+                        (fun (ra, st) (ru, _) ->
+                          if ra == ru then (ru, ru) else (ru, st)
+                        ) (-1, -1) cs
+                    ) (List.map (List.sort cRank) css) |> List.map snd
       in let maxV = List.max ranks 
          in List.mapi (fun i x -> if x == maxV then i else -1) ranks |>
               List.filter (fun x -> x > -1) |> List.map (List.nth css) |> List.map (List.take 2)
 
+    let rangeDupal =
+      fun css ->
+      let ranks = List.map (fun cs ->
+                    List.fold_left
+                      (fun (ra, st) (ru,_) ->
+                        if ra = ru then (ru, ru :: st) else (ru, st)
+                      ) (-1, []) cs
+         ) (List.map (List.sort cRank) css) |> List.map snd |>
+                    List.map (fun xs -> List.sort compare xs |> List.rev)
+      in List.mapi (fun i xs -> if List.length xs == 2 then i else -1) ranks |> 
+           List.filter (fun x -> x > -1) |> List.map (List.nth css) |> List.map (List.take 2) 
+      
   end ;;
 
 
   (* test suite *)
 let board = [(3,1);(7,3);(0,1);(5,1);(2,1)] ;;
-let tu2 = Rangir.rangePairs
+let tu2 = Rangir.rangeDupal
             [
-              [( 4, 1); ( 1, 1)]   @ board ;
-              [( 2, 0); ( 6, 1)]   @ board ;
-              [( 4, 3); ( 11, 3)]   @ board ;
+              [( 4, 1); ( 1, 1)] @ board ;
+              [( 2, 0); ( 5, 1)] @ board ;
+              [( 0, 3); ( 7, 2)] @ board ;
             ]
