efc4a0363a562302049f005d70f2c9f4a3e093cd
Your Name <you@example.com>
2018-12-14 05:18:21 +0000

    ok start with High - do half way

--- TH/rangir.ml
+++ TH/rangir.ml
@@ -1,6 +1,29 @@
+open Batteries ;;
+
+exception Bad_list_of_cards ;;
+  
 module Rangir =
   struct
+    
     type cards = int * int list
 
+    let rangeHigh =
+      fun css ->
+      let pockets = List.map (fun cs -> List.take 2 cs) css
+      and maxRank = List.map (fun cs -> List.max cs) css |> List.max
+      in let rez = List.filter
+                     (fun xs -> not (List.is_empty (List.filter (fun x -> x == maxRank) xs)))
+                     pockets
+            in if List.length rez > 0 then rez else pockets 
+    ;; 
+                      
     let rangePairs css = css
+                       
   end ;;
+
+let tu2 = Rangir.rangeHigh 
+            [
+              [(4,1);(6,1);   (3,1);(7,3);(8,1);(5,1);(2,1)] ;
+              [(11,1);(2,2);   (3,1);(7,3);(8,1);(5,1);(2,1)] ;
+              [(11,0);(11,3);  (3,1);(7,3);(8,1);(5,1);(2,1)] ;              
+            ]
--- TH/rangir.mli
+++ TH/rangir.mli
@@ -2,5 +2,5 @@ module Rangir :
   sig
     type cards = int * int list
                
-    val rangePairs : cards list list -> cards list list
+    (* val rangePairs : cards list list -> cards list list *)
   end ;;
