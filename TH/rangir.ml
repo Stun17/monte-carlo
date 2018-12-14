8100810d8d67ab25e91a5f47aada39d7afef007d
Your Name <you@example.com>
2018-12-14 06:49:44 +0000

    bad way

--- TH/rangir.ml
+++ TH/rangir.ml
@@ -7,23 +7,36 @@ module Rangir =
     
     type cards = int * int list
 
+    let ranksort =
+      fun [(r1,_);(r2,_)] [(k1,_);(k2,_)] ->
+                     if r1 > k1 && r1 > k2 then 1 else
+                       if r2 > k1 && r2 > k2 then 1 else
+                         if r1 == k1 && r2 == k2 then 0 else
+                           if r1 == k2 && r2 == k1 then 0 else -1
+                      
     let rangeHigh =
       fun css ->
       let pockets = List.map (fun cs -> List.take 2 cs) css
-      and maxRank = List.map (fun cs -> List.max cs) css |> List.max
-      in let rez = List.filter
-                     (fun xs -> not (List.is_empty (List.filter (fun x -> x == maxRank) xs)))
-                     pockets
-            in if List.length rez > 0 then rez else pockets 
+      and (maxRank,_) = List.map (fun cs -> List.max cs) css |> List.max
+                      (* pockets list never will be empty so ignore warning  *)
+      in let rez = List.filter (fun [(r1,_); (r2,_)] -> r1 == maxRank || r2 == maxRank) pockets
+         in if List.length rez > 0
+            then List.sort ranksort rez 
+            else List.sort ranksort pockets 
     ;; 
                       
     let rangePairs css = css
                        
   end ;;
 
-let tu2 = Rangir.rangeHigh 
+
+  (* test suite *)
+let board = [(3,1);(7,3);(0,1);(5,1);(2,1)] ;;
+
+(* Rangir.ranksort  *)
+let tu2 = Rangir.rangeHigh
             [
-              [(4,1);(6,1);   (3,1);(7,3);(8,1);(5,1);(2,1)] ;
-              [(11,1);(2,2);   (3,1);(7,3);(8,1);(5,1);(2,1)] ;
-              [(11,0);(11,3);  (3,1);(7,3);(8,1);(5,1);(2,1)] ;              
+              [( 4, 1); ( 1, 1)]   @ board ;
+              [( 6, 3); ( 2, 2)]   @ board ;
+              [( 1, 0); ( 6, 1)]   @ board ;
             ]
