7e2ee362249c24bebf2b17b9907bb74385542e7e
Your Name <you@example.com>
2018-12-14 09:01:49 +0000

    ok good with high range

--- TH/rangir.ml
+++ TH/rangir.ml
@@ -7,24 +7,29 @@ module Rangir =
     
     type cards = int * int list
 
-    let ranksort =
-      fun [(r1,_);(r2,_)] [(k1,_);(k2,_)] ->
-                     if r1 > k1 && r1 > k2 then 1 else
-                       if r2 > k1 && r2 > k2 then 1 else
-                         if r1 == k1 && r2 == k2 then 0 else
-                           if r1 == k2 && r2 == k1 then 0 else -1
-                      
+    let pRank =
+      fun xs ->
+      match xs with
+      | [(r1,_); (r2,_)] -> if r1 >= r2 then xs else List.rev xs 
+      | _ -> xs
+
+           
     let rangeHigh =
       fun css ->
-      let pockets = List.map (fun cs -> List.take 2 cs) css
-      and (maxRank,_) = List.map (fun cs -> List.max cs) css |> List.max
-                      (* pockets list never will be empty so ignore warning  *)
-      in let rez = List.filter (fun [(r1,_); (r2,_)] -> r1 == maxRank || r2 == maxRank) pockets
-         in if List.length rez > 0
-            then List.sort ranksort rez 
-            else List.sort ranksort pockets 
-    ;; 
-                      
+      let rez1 = List.map (fun cs -> List.take 2 cs) css |>
+                   List.map pRank |> List.sort compare |> List.rev
+      in let maxRank = List.max rez1 |> List.hd |> fst
+         in let rez2 = List.take_while (fun [(r1,_); _] -> r1 == maxRank) rez1
+            in List.fold_left (
+                   fun (v, st) xs ->
+                   match xs with
+                   | [_ ; (r2, _)] ->
+                      if r2 > v then (r2, [xs]) else
+                        if r2 == v then (v, xs :: st) else
+                          (v, st)
+                   | _ -> (v, st)
+                 ) (0, []) rez2 |> snd 
+            
     let rangePairs css = css
                        
   end ;;
@@ -37,6 +42,6 @@ let board = [(3,1);(7,3);(0,1);(5,1);(2,1)] ;;
 let tu2 = Rangir.rangeHigh
             [
               [( 4, 1); ( 1, 1)]   @ board ;
-              [( 6, 3); ( 2, 2)]   @ board ;
-              [( 1, 0); ( 6, 1)]   @ board ;
+              [( 6, 1); ( 5, 0)]   @ board ;
+              [( 2, 3); ( 6, 3)]   @ board ;
             ]
