21d1b7973736c3a2bea3e84df700c776cb6285aa
Your Name <you@example.com>
2019-07-09 18:00:29 +0000

    oki in NixOS 19 now

--- TH/arbitrage.ml
+++ TH/arbitrage.ml
@@ -6,12 +6,14 @@ open Decisions ;; open Batteries ;; open Printf ;;
 type hand = (int * int) list  
 type cards = hand list ;;
 
+(* just output procedure - nothing special here *)
 let print_hand xs title =
   printf "%s\t" title ;
   List.iter (fun (x,y) -> printf "%2i %i\t" x y) xs ;
   printf "\n"
 ;;
 
+(*  obtain the highest rank of the hand *)
 let getHigh xs =
   List.sort compare xs |> List.rev |> List.split |> fst |> List.hd
 ;;
@@ -52,58 +54,60 @@ let kicker xs ys title =
   let one = List.sort compare xs |> List.rev and two = List.sort compare ys |> List.rev in
   match compare (getHigh one) (getHigh two) with
   | -1 -> print_hand ys title | 1 -> print_hand xs title | 0 ->
-     (match compare (List.tl one |> getHigh) (List.tl two |> getHigh) with
+   (match compare (List.tl one |> getHigh) (List.tl two |> getHigh) with
+    | -1 -> print_hand ys title | 1 -> print_hand xs title | 0 ->
+    (match compare (List.drop 2 one |> getHigh) (List.drop 2 two |> getHigh) with
+     | -1 -> print_hand ys title | 1 -> print_hand xs title | 0  ->
+     (match compare (List.drop 3 one |> getHigh) (List.drop 3 two |> getHigh) with
       | -1 -> print_hand ys title | 1 -> print_hand xs title | 0 ->
-         (match compare (List.drop 2 one |> getHigh) (List.drop 2 two |> getHigh) with
-          | -1 -> print_hand ys title | 1 -> print_hand xs title | 0  ->
-             (match compare (List.drop 3 one |> getHigh) (List.drop 3 two |> getHigh) with
-              | -1 -> print_hand ys title | 1 -> print_hand xs title | 0 ->
-                 (match compare (List.drop 4 one |> getHigh) (List.drop 4 two |> getHigh) with
-                  | -1 -> print_hand ys title | 1 -> print_hand xs title | 0 ->
-                     (match compare (List.drop 5 one |> getHigh) (List.drop 5 two |> getHigh) with
-                      | -1 -> print_hand ys title | 1 -> print_hand xs title | 0 ->
-                         (match compare (List.drop 6 one |> getHigh) (List.drop 6 two |> getHigh) with
-                          | -1 -> print_hand ys title | 1 -> print_hand xs title | 0 ->
-                             print_hand xs title ; print_hand ys title ))))))
+      (match compare (List.drop 4 one |> getHigh) (List.drop 4 two |> getHigh) with
+       | -1 -> print_hand ys title | 1 -> print_hand xs title | 0 ->
+       (match compare (List.drop 5 one |> getHigh) (List.drop 5 two |> getHigh) with
+        | -1 -> print_hand ys title | 1 -> print_hand xs title | 0 ->
+        (match compare (List.drop 6 one |> getHigh) (List.drop 6 two |> getHigh) with
+         | -1 -> print_hand ys title | 1 -> print_hand xs title | 0 ->
+            print_hand xs title ; print_hand ys title ))))))
 ;;
 
 let arbitThem xs title =
-  let one = List.take 7 xs 
-  and two = List.drop 7 xs |> List.take 7 
-  in match title with
-     | "high" ->
-        kicker one two title
-     | "pair" ->
-        (match compare (getPair one) (getPair two) with
-         | -1 -> print_hand two title | 1 -> print_hand one title 
-         | 0 -> kicker one two title )
-     | "dupal" ->
-        (match compare (getDupal one) (getDupal two) with
-         | -1 -> print_hand two title | 1 -> print_hand one title
-         | 0 -> kicker one two title )
-     | "set" ->
-        (match compare (getSet one) (getSet two) with
-         | -1 -> print_hand two title | 1 -> print_hand one title
-         | 0 -> kicker one two title )           
-     | "str8" ->
-        (match compare (getStr8 one) (getStr8 two) with
-         | -1 -> print_hand two title | 1 -> print_hand one title
-         | 0 -> kicker one two title )              
-     | "flush"  -> () 
-     | "full"   -> () 
-     | "caree"  -> ()   
-     | "fl-st"  -> ()
-     | _        -> ()
+  let one = List.take 7 xs and two = List.drop 7 xs |> List.take 7 in
+  match title with
+  | "high" ->
+     kicker one two title
+  | "pair" ->
+     (match compare (getPair one) (getPair two) with
+      | -1 -> print_hand two title | 1 -> print_hand one title 
+      | 0 -> kicker one two title )
+  | "dupal" ->
+     (match compare (getDupal one) (getDupal two) with
+      | -1 -> print_hand two title | 1 -> print_hand one title
+      | 0 -> kicker one two title )
+  | "set" ->
+     (match compare (getSet one) (getSet two) with
+      | -1 -> print_hand two title | 1 -> print_hand one title
+      | 0 -> kicker one two title )           
+  | "str8" ->
+     (match compare (getStr8 one) (getStr8 two) with
+      | -1 -> print_hand two title | 1 -> print_hand one title
+      | 0 -> kicker one two title )              
+  | "flush"  -> () 
+  | "full"   -> () 
+  | "caree"  -> ()   
+  | "fl-st"  -> ()
+  | _        -> ()
 ;;
   
 let myWorkFun cs predicat title continuation =
   match (List.map predicat cs |> List.flatten) with
-  | [] ->
-     continuation cs
-  | xs ->
-     if (List.length xs = 7)
+  | [] ->                       (* we dont have any coincidence on this step *)
+     continuation cs            (* so we continue in next low level *)
+  | xs ->                       (* we have some result and treat them  *)
+     if (List.length xs = 7)    (* if we have single result - we print it *)
      then print_hand xs title
-     else arbitThem xs title
+     else                       (* if we have multiple results we treat them differently *)
+       if (List.length xs = 14)
+       then arbitThem xs title
+       else print_endline (title ^ " three or more")
 ;;
 
 let isAnyHaveHigh  = fun cs -> myWorkFun cs isHigh      "high"  (fun cs -> ()) ;;
@@ -117,20 +121,23 @@ let isAnyHaveCaree = fun cs -> myWorkFun cs isCaree     "caree" isAnyHaveFull  ;
 let isAnyHaveFlStr = fun cs -> myWorkFun cs isFlushStr8 "fl-st" isAnyHaveCaree ;;
   
 let start cs =
-  let ts = List.hd cs |> List.drop 2 
-  in if isColor ts
-     then isAnyHaveFlStr cs
-     else
-       if isDry ts
-       then isAnyHaveCaree cs
-       else isAnyHaveStr cs ;;
+  let ts = List.hd cs |> List.drop 2 (*  we took the board cards only  *)
+  in if isColor ts                   (* and chek if they have three in suit *)
+     then isAnyHaveFlStr cs          (* and in this case we start check from the begining *)
+     else                            (* else we skip the first step in checking *)
+       if isDry ts                   (* and check if board have set *)
+       then isAnyHaveCaree cs        (* in which case we check caree and full *)
+       else isAnyHaveStr cs ;;       (* else we start from stright  *)
        
 end ;;                                    
 
 
-(* test suite  *)
-  open Arbitrage ;;
+(* ------------------------  test suite --------------------------  *)
+  
+open Arbitrage ;;
+  
 let xs = [(11,1) ; (9,2) ; (7,0) ; (5,3) ; (6,3) ; (4,1) ; (3,0) ] ;;
+  
 (* let teHigh = getHigh  xs ;; *)
 (* let tePair = getPair xs ;; *)
 (* let teDupal = getDupal xs ;; *)
