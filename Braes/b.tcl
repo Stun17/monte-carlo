c28a9c894acebebd8bc17096c5193bd9bf499408
Your Name <you@example.com>
2019-07-10 19:37:42 +0000

    with grid placement in Baers

--- Braes/b.tcl
+++ Braes/b.tcl
@@ -7,44 +7,37 @@ proc calculateit { } {
     global limit price init1 init2
 
     set rez [exec "./a.out" $price $limit $init1 $init2]
-    .z.l1 configure -text "распределение: $rez"
+    .f.l1 configure -text "распределение: $rez"
 }
 
 #-----------------------------------------------------------------------
 
 labelframe .f -text "исходные данные" 
 
-labelframe .f.llim -text "пропускная\nспособность" 
+label .f.ll -text "пропускная\nспособность" 
 
-listbox .f.llim.lb -yscrollcommand {.f.llim.sb set} -height 4 -width 2 \
-  -listvariable limits -selectmode single
-.f.llim.lb activate 2 
-.f.llim.lb see 2 
-.f.llim.lb selection set 0 
-bind .f.llim.lb <<ListboxSelect>> { setLimit [%W curselection] }
+listbox .f.lb -height 9 -width 3 -listvariable limits -selectmode single
+.f.lb activate 2 
+.f.lb see 2 
+.f.lb selection set 0 
+bind .f.lb <<ListboxSelect>> { setLimit [%W curselection] }
 
-scrollbar .f.llim.sb -orient vertical -command {.f.llim.lb yview}
-
-frame .f.fc
-
-scale .f.fc.sc1 -label цена   -variable price -orient horizontal -from -22 -to 22 \
+scale .f.s1 -label цена   -variable price -orient horizontal -from -22 -to 22 \
   -length 120 -showvalue 1 
-scale .f.fc.sc2 -label "нач.условия Х" -variable init1 -orient horizontal -from 0 -to [expr "$limit/2"] \
+scale .f.s2 -label "нач.условия Х" -variable init1 -orient horizontal -from 0 -to [expr "$limit/2"] \
   -length 120 -showvalue 1 
-scale .f.fc.sc3 -label "нач.условия Y" -variable init2 -orient horizontal -from 0 -to [expr "$limit/2"] \
+scale .f.s3 -label "нач.условия Y" -variable init2 -orient horizontal -from 0 -to [expr "$limit/2"] \
   -length 120 -showvalue 1 
 
 proc setLimit { num } {
   global limit 
 
-  set limit [.f.llim.lb get $num]
-  .f.fc.sc2 configure -to [expr "$limit / 2 - 1"]
-  .f.fc.sc3 configure -to [expr "$limit / 2 - 1"]
+  set limit [.f.lb get $num]
+  .f.s2 configure -to [expr "$limit / 2 - 1"]
+  .f.s3 configure -to [expr "$limit / 2 - 1"]
 }
 
-frame .g
-
-label .g.lb2 -justify left -wraplength 590 -font myfont -text "есть две дороги из А в В - одна через пункт 1 и другая через пункт 2. цена за проезд из А в 1 постоянна и равна 10. такая же цена за проезд из 2 в В. цены за проезд из 1 в В и из А в 2 зависят от текущего количества машин, выбравших то или иное направление и расчитывается по формуле:\n price(1B) = 20 * x / (x + y) ; price(A2) = 20 * y / (x + y), \nгде x - число машин выбравших путь через 1, а у - число машин, выбравших путь через 2\n тут открыли дорогу из 1 в 2 и установили плату (от -22 до +22). как размер платы за новую дорогу повлияет на распределение потока машин?"
+label .t -justify left -wraplength 590 -font myfont -text "\nесть две дороги из А в В - одна через пункт 1 и другая через пункт 2. цена за проезд из А в 1 постоянна и равна 10. такая же цена за проезд из 2 в В. цены за проезд из 1 в В и из А в 2 зависят от текущего количества машин, выбравших то или иное направление и расчитывается по формуле:\n price(1B) = 20 * x / (x + y) \n price(A2) = 20 * y / (x + y), \nгде x - число машин, выбравших путь через 1\n    у - число машин, выбравших путь через 2\nтут открыли дорогу из 1 в 2 и установили плату (от -22 до +22)\nкак размер платы за новую дорогу повлияет на распределение потока машин?"
 
 frame .f.fb 
 
@@ -58,43 +51,36 @@ proc reset { } {
   set limit 30
   set init1 5
   set init2 5 
-  set n [.f.llim.lb curselection] 
-  .f.llim.lb selection clear $n 
-  .f.llim.lb activate 2 
-  .f.llim.lb see 2 
-  .f.llim.lb selection set 2 
-  .f.fc.sc2 configure -to 14 
-  .f.fc.sc3 configure -to 14 
-  .z.l1 configure -text "нет результатов!" 
+  set n [.f.lb curselection] 
+  .f.lb selection clear $n 
+  .f.lb activate 2 
+  .f.lb see 2 
+  .f.lb selection set 2 
+  .f.s2 configure -to 14 
+  .f.s3 configure -to 14 
+  .f.l1 configure -text "нет результатов!" 
 }
 
 #-----------------------------------------------------------------------
 
-labelframe .z -text ответ 
-label .z.l1 -text "нет результатов!" -width 30 
+label .f.l1 -text "нет результатов!" -width 30 -font { -size 12 } -fg navy 
 
 image create photo diag -file "diagram.png" 
 label .p -image diag
 
-button .b3 -command exit -text "выйти" -width 15
+button .f.b3 -command exit -text "выйти" -width 15
 
 #-----------------------------------------------------------------------
 
-place .g -x 20 -y 20 ; pack .g.lb2 -padx 20 -pady 10 ; place .p -x 20 -y 250 
+place .t -x  20 -y 330  
+place .p -x 650 -y 330 
 
-pack .b3 -side right -padx 10 -pady 10 -fill none -anchor se 
+grid .f -column 0 -row 0 -padx 30 -pady 30
 
-pack .f -side right -padx 20 -pady 80 -anchor nw
-pack .z -side bottom -padx 10 -pady 10 -expand no -anchor se
-
-pack .f.llim -side left -padx 25 -pady 15 
-pack .f.llim.lb -side left -padx 0 -pady 15 -expand yes 
-pack .f.llim.sb -side left -padx 0 -pady 15 -fill y -expand yes 
-
-pack .f.fc -side left 
-pack .f.fc.sc1 .f.fc.sc2 .f.fc.sc3 -side top -padx 20 -pady 5
-
-pack .f.fb -side left -padx 20 
-pack .f.fb.b1 .f.fb.b2 -side top 
-
-pack .z.l1 -padx 0 -pady 10 -anchor sw 
+grid .f.lb -column 0 -rowspan 3 
+grid .f.l1 -column 2 -row 1 
+grid .f.s1 -column 1 -row 0 
+grid .f.s2 -column 1 -row 1
+grid .f.s3 -column 1 -row 2
+grid .f.fb -column 0 -row 3 ; grid .f.fb.b1 .f.fb.b2 -pady 20 -padx 20
+grid .f.b3 -column 2 -row 3 -padx 20 -pady 20 
