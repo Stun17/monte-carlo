7b7abf4ae58bcd5d171d0589aa520d39b557d557
Your Name <you@example.com>
2019-07-10 15:28:44 +0000

    done with Braes paradox

--- Braes/Braes.hs
+++ Braes/Braes.hs
@@ -3,12 +3,11 @@ module Main where
 import Control.Monad.State
 import System.Environment
 
-
--- input  :  price for additional path
---           num of vehicles
---           initial state vehicles01
---           initial state vehicles02
--- output :  (vehicles01, vehicles02)
+-- input  :  price for additional middle path
+--           max num of vehicles on all paths 
+--           initial state - num of vehicles to point 1
+--           initial state - num of vehicles to point 2
+-- output :  resulting division (vehicles to 1, vehicles to 2)
 main :: IO ()
 main =
   getArgs >>= \ (prc:lim:ini1:ini2:_) ->
@@ -18,38 +17,54 @@ main =
       price  = read prc
       count  = init01 + init02
   in  guard (limit > count) >> -- prg must terminate
-      (print $ doitnow limit price (count, (init01, init02)))
+      (print $ calculate limit price (count, (init01, init02)))
 
 type Traffic = (Integer, Integer)
 
-doitnow :: Integer -> Double -> (Integer, Traffic) -> Traffic
-doitnow k price33 (n, g@(st1, st2))
-  | n >= k = g
-  | otherwise = doitnow k price33 $ runState (action price33) g
+calculate :: Integer -> Double -> (Integer, Traffic) -> Traffic
+calculate limit price3 (count, state@(state1, state2))
+  | count >= limit = state 
+  | otherwise = calculate limit price3 $ runState (action price3) state 
 
--- price for path
-price11, price12, price21, price22 :: Traffic -> Double
-price11 _ = 10
-price12 (x, y) = 20 * (fromInteger x) / (fromInteger $ x + y)
-price21 (x, y) = 20 * (fromInteger y) / (fromInteger $ x + y)
-price22 _ = 10
+-- prices for paths
+price_a1, price_1b, price_a2, price_2b :: Traffic -> Double
+price_a1 _ = 10
+price_1b (x, y) = 20 * (fromInteger x) / (fromInteger $ x + y)
+price_a2 (x, y) = 20 * (fromInteger y) / (fromInteger $ x + y)
+price_2b _ = 10
 
--- which path is cheaper
+-- which path is cheaper?
 decision :: Double -> Traffic -> Bool
-decision price33 x =
-  let path11  = price11 x + price12 x
-      path12  = price11 x + price33 + price22 x
-      path21  = price21 x + price22 x
-      path22  = price21 x + price33 + price12 x
-      ps      = [path11, path12, path21, path22]
-      minpath = minimum ps
-      alloc   = map (\x -> if x > minpath then 0 else 1) ps
+decision price3 s =
+  let path_a1b  = price_a1 s + price_1b s
+      path_a12b = price_a1 s + price3 + price_2b s
+      path_a2b  = price_a2 s + price_2b s
+      path_a21b = price_a2 s + price3 + price_1b s
+      ps        = [path_a1b, path_a12b, path_a2b, path_a21b]
+      minpath   = minimum ps
+      alloc     = map (\z -> if z > minpath then 0 else 1) ps
   in case alloc of
-    [_,_,0,0] -> True
+    [1,0,0,0] -> True
+    [0,1,0,0] -> True 
+    [0,0,1,0] -> False 
+    [0,0,0,1] -> False 
+    [1,1,0,0] -> True
+    [1,0,1,0] -> True 
+    [1,0,0,1] -> False 
+    [0,1,1,0] -> False 
+    [0,1,0,1] -> True 
+    [0,0,1,1] -> False 
+    [1,1,1,0] -> True
+    [1,1,0,1] -> True
+    [1,0,1,1] -> False 
+    [0,1,1,1] -> False 
+    [1,1,1,1] -> True 
     _         -> False
 
 action :: Double -> State Traffic Integer
-action price33 =  
-  get >>= \ st@(st1, st2) ->
-  (if decision price33 st then put (st1 + 1, st2) else put (st1, st2 + 1)) >>
-  return (st1 + st2 + 1)
+action price3 =  
+  get >>= \ state@(state1, state2) ->
+  ( if decision price3 state 
+    then put (state1 + 1, state2) 
+    else put (state1, state2 + 1) ) >>
+  return (state1 + state2 + 1)
--- Braes/b.tcl
+++ Braes/b.tcl
@@ -1,67 +1,95 @@
 #!/run/current-system/sw/bin/wish
 
-set price 1
-set limit 30
-set init1 5
-set init2 5 
+set limits [list 10 20 30 40 50 60 70 80 90 100]
+set price 1 ; set limit 30 ; set init1 5 ; set init2 5 
 
-proc doit { } {
+proc calculateit { } {
     global limit price init1 init2
 
     set rez [exec "./a.out" $price $limit $init1 $init2]
-    .lans.l1 configure -text "распределение: $rez"
+    .z.l1 configure -text "распределение: $rez"
 }
 
-frame .f9 
+#-----------------------------------------------------------------------
 
-labelframe .f9.llim -text "пропускная\nспособность" 
-listbox .f9.llim.lb -yscrollcommand {.f9.llim.s set} -height 5 -width 5
-for {set i 10} { $i < 101 } { incr i 5} {.f9.llim.lb insert end $i} 
-scrollbar .f9.llim.s -orient vertical -command {.f9.llim.lb yview}
-bind .f9.llim.lb <<ListboxSelect>> { setLimit [%W curselection] }
+labelframe .f -text "исходные данные" 
 
-scale .f9.t1 -label цена -variable price -orient horizontal -from 0 -to 20  -length 100 -showvalue 1 
-scale .f9.t2 -label стартХ -variable init1 -orient horizontal -from 0 -to [expr "$limit/2"] -length 100 -showvalue 1 
-scale .f9.t3 -label стартУ -variable init2 -orient horizontal -from 0 -to [expr "$limit/2"] -length 100 -showvalue 1 
+labelframe .f.llim -text "пропускная\nспособность" 
+
+listbox .f.llim.lb -yscrollcommand {.f.llim.sb set} -height 5 -width 5 -listvariable limits \
+  -selectmode single
+.f.llim.lb activate 2 
+.f.llim.lb see 2 
+.f.llim.lb selection set 0 
+bind .f.llim.lb <<ListboxSelect>> { setLimit [%W curselection] }
+
+scrollbar .f.llim.sb -orient vertical -command {.f.llim.lb yview}
+
+frame .f.fc
+
+scale .f.fc.sc1 -label цена   -variable price -orient horizontal -from -2 -to 22 \
+  -length 120 -showvalue 1 
+scale .f.fc.sc2 -label стартХ -variable init1 -orient horizontal -from 0 -to [expr "$limit/2"] \
+  -length 120 -showvalue 1 
+scale .f.fc.sc3 -label стартУ -variable init2 -orient horizontal -from 0 -to [expr "$limit/2"] \
+  -length 120 -showvalue 1 
 
 proc setLimit { num } {
   global limit 
 
-  set limit [.f9.llim.lb get $num]
-  .f9.t2 configure -to [expr "$limit / 2 - 1"]
-  .f9.t3 configure -to [expr "$limit / 2 - 1"]
+  set limit [.f.llim.lb get $num]
+  .f.fc.sc2 configure -to [expr "$limit / 2 - 1"]
+  .f.fc.sc3 configure -to [expr "$limit / 2 - 1"]
 }
 
+frame .f.fb 
 
-frame   .f9.f0 
-button  .f9.f0.b1 -command doit  -text "посчитать"  -width 10
-button  .f9.f0.b2 -command reset -text "в исходное" -width 10
-button  .f9.f0.b3 -command exit  -text "выйти"  -width 10
+button .f.fb.b1 -command calculateit  -text "посчитать"  -width 10
+button .f.fb.b2 -command reset -text "в исходное" -width 10
 
-proc reset {} {
+proc reset { } {
   global limit init1 init2 price
 
   set price 1
   set limit 30
   set init1 5
   set init2 5 
-  .f9.t2 configure -to 14 
-  .f9.t3 configure -to 14 
+  set n [.f.llim.lb curselection] 
+  .f.llim.lb selection clear $n 
+  .f.llim.lb activate 2 
+  .f.llim.lb see 2 
+  .f.llim.lb selection set 2 
+  .f.fc.sc2 configure -to 14 
+  .f.fc.sc3 configure -to 14 
+  .z.l1 configure -text "нет результатов!" 
 }
 
-labelframe .lans -text ответ 
-label .lans.l1 -text "нет результатов!" -width 25
+#-----------------------------------------------------------------------
+
+labelframe .z -text ответ 
+label .z.l1 -text "нет результатов!" -width 40 
 
 image create photo diag -file "diagram.png" 
 label .p -image diag
 
-pack .p -padx 20 -pady 30 
-pack .f9
-pack .f9.llim -side left -padx 25 
-pack .f9.llim.lb -side left -padx 1 -pady 15 -expand yes 
-pack .f9.llim.s -side left -padx 5 -pady 15 -fill y
-pack .f9.t1 .f9.t2 .f9.t3 -side left -padx 20
-pack .f9.f0 -side left -padx 10 
-pack .f9.f0.b1 .f9.f0.b2 .f9.f0.b3 -side top 
-pack .lans -padx 100 -pady 20 -fill x  
-pack .lans.l1 -padx 10 -pady 10 
+button .b3 -command exit -text "выйти" -width 15
+
+#-----------------------------------------------------------------------
+
+pack .b3 -side top  -padx 180 -pady 30 -fill none -anchor nw 
+
+pack .f  -side top  -padx 20  -pady 10            -anchor nw
+pack .z  -side left -padx 50  -pady 20 -fill x    -anchor nw 
+pack .p  -side top  -padx 20  -pady 10 
+
+pack .f.llim -side left -padx 25 -pady 15 
+pack .f.llim.lb -side left -padx 1 -pady 15 -expand yes 
+pack .f.llim.sb -side left -padx 5 -pady 15 -fill y
+
+pack .f.fc -side left 
+pack .f.fc.sc1 .f.fc.sc2 .f.fc.sc3 -side top -padx 20 -pady 5
+
+pack .f.fb -side left -padx 20 
+pack .f.fb.b1 .f.fb.b2 -side top 
+
+pack .z.l1 -padx 10 -pady 10 
