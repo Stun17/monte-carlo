27431bc9c13870a778e61cd4eb0810da3c0dceca
Your Name <you@example.com>
2019-11-09 03:28:17 +0000

    with evaluations now - good raw draft

--- TH/evaluations.ml
+++ TH/evaluations.ml
@@ -12,10 +12,10 @@ module Evaluations =
     let getPair xs =
       List.sort compare xs |> List.rev |> List.split |> fst |>
         fun ys ->
-        List.fold_right (
-            fun x (a, b) ->
-            if x = b then (x, x) else (a, x))
-                        (List.tl ys) (List.hd ys, 0) 
+        List.fold_right
+          (fun x acc -> if x == acc then acc else x)
+          (List.tl ys)
+          (List.hd ys) 
     ;;
 
     let getDupal xs =
@@ -25,16 +25,14 @@ module Evaluations =
         and ys2 = 0 :: ys
         in List.map2 (fun s1 s2 -> if s1 = s2 then s1 else 0) ys1 ys2 |>
              List.sort compare |> List.rev |>
-             fun zs -> (List.hd zs, List.tl zs |> List.hd)
+             fun zs -> 10 * (List.hd zs) + (List.hd (List.tl zs))
     ;;
 
     let getSet xs =
       List.sort compare xs |> List.rev |> List.split |> fst |>
         fun ys ->
-        let ys1 = ys @ [0]
-        and ys2 = 0 :: ys
-        in List.map2 (fun s1 s2 -> if s1 = s2 then s1 else 0) ys1 ys2 |>
-             List.sort compare |> List.rev |> List.hd
+        List.map2 (fun s1 s2 -> if s1 = s2 then s1 else 0) (ys @ [0]) (0 :: ys) |>
+          List.sort compare |> List.rev |> List.hd
     ;;
 
     let getStr8 xs =
@@ -45,95 +43,57 @@ module Evaluations =
         in List.map2 (fun s1 s2 -> if s2 - s1 = 1 then s2 else 0) ys1 ys2 |>
              List.sort compare |> List.rev |> List.hd  
 
-    let kicker xs ys title =
+    let getFlush xs     = 0 ;;
+    let getFull xs      = 0 ;;
+    let getCaree xs     = 0 ;;
+    let getFlushStr8 xs = 0 ;;
+      
+    let rec kicker xs ys title =
       let one = List.sort compare xs |> List.rev 
       and two = List.sort compare ys |> List.rev 
       in
-      match
-        compare (getHigh one) (getHigh two)
-      with
+        match compare (getHigh one) (getHigh two) with
         | -1 -> ()
         |  1 -> ()
-        | _  ->
-           (match
-              compare (List.tl one |> getHigh) (List.tl two |> getHigh)
-            with
-            | -1 -> ()
-            |  1 -> ()
-            | _  ->
-               (match
-                  compare (Bat.drop 2 one |> getHigh) (Bat.drop 2 two |> getHigh)
-                with
-                | -1 -> ()
-                |  1 -> ()
-                | _  ->
-                   (match
-                      compare (Bat.drop 3 one |> getHigh) (Bat.drop 3 two |> getHigh)
-                    with
-                    | -1 -> ()
-                    |  1 -> ()
-                    | _  ->
-                       (match
-                          compare (Bat.drop 4 one |> getHigh) (Bat.drop 4 two |> getHigh)
-                        with
-                        | -1 -> ()
-                        |  1 -> ()
-                        | _  ->
-                           (match
-                              compare (Bat.drop 5 one |> getHigh) (Bat.drop 5 two |> getHigh)
-                            with
-                            | -1 -> ()
-                            |  1 -> ()
-                            | _  ->
-                               (match
-                                  compare (Bat.drop 6 one |> getHigh) (Bat.drop 6 two |> getHigh)
-                                with
-                                | -1 -> ()
-                                |  1 -> ()
-                                | _  -> () ; () )
-                           )
-                       )
-                   )
-               )
-           )
-   ;;
+        |  _ -> kicker (List.tl one) (List.tl two) title
+    ;;
 
    let arbitThem xs title =
      let one = Bat.take 7 xs and two = Bat.drop 7 xs |> Bat.take 7 in
      match title with
-     | 900 ->
-        kicker one two title
-     | 800 ->
-        (match
-           compare (getPair one) (getPair two)
-         with
-         | -1 -> ()
-         | 1 -> ()
-         | _ -> kicker one two title )
-     | 700 ->
-        (match
-           compare (getDupal one) (getDupal two)
-         with
-         | -1 -> ()
-         |  1 -> ()
-         | _ -> kicker one two title )
-     | 600 ->
-        (match
-           compare (getSet one) (getSet two)
-         with
-         | -1 -> ()
-         |  1 -> ()
-         | _ -> kicker one two title )           
-     | 500 ->
-        (match
-           compare (getStr8 one) (getStr8 two)
-         with
-         | -1 -> () | 1 -> ()
-         | _  -> kicker one two title )              
-     | 400  -> () 
-     | 300  -> () 
-     | 200  -> ()   
-     | 100  -> ()
+     | 900 -> kicker one two title
+     | 800 -> (match compare (getPair one) (getPair two) with
+               | -1 -> ()
+               |  1 -> ()
+               |  _ -> kicker one two title )
+     | 700 -> (match compare (getDupal one) (getDupal two) with
+               | -1 -> ()
+               |  1 -> ()
+               |  _ -> kicker one two title )
+     | 600 -> (match compare (getSet one) (getSet two) with
+               | -1 -> ()
+               |  1 -> ()
+               |  _ -> kicker one two title )           
+     | 500 -> (match compare (getStr8 one) (getStr8 two) with
+               | -1 -> ()
+               |  1 -> ()
+               |  _ -> kicker one two title )              
+     | 400  -> (match compare (getFlush one) (getFlush two) with
+                | -1 -> ()
+                |  1 -> ()
+                |  _ -> kicker one two title ) 
+     | 300  -> (match compare (getFull one) (getFull two) with
+                | -1 -> ()
+                |  1 -> ()
+                |  _ -> kicker one two title ) 
+     | 200  -> (match compare (getCaree one) (getCaree two) with
+                | -1 -> ()
+                |  1 -> ()
+                |  _ -> kicker one two title ) 
+     | 100  -> (match compare (getFlushStr8 one) (getFlushStr8 two) with
+                | -1 -> ()
+                |  1 -> ()
+                |  _ -> kicker one two title ) 
      | _    -> ()
    ;;
      
--- TH/evaluations.mli
+++ TH/evaluations.mli
@@ -3,11 +3,15 @@ module Evaluations :
 
     type hand = (int * int) list
               
-    val getHigh   : hand -> int 
-    val getPair   : hand -> int * int 
-    val getDupal  : hand -> int * int
-    val getSet    : hand -> int 
-    val getStr8   : hand -> int 
+    val getHigh      : hand -> int 
+    val getPair      : hand -> int
+    val getDupal     : hand -> int
+    val getSet       : hand -> int 
+    val getStr8      : hand -> int 
+    val getFlush     : hand -> int
+    val getFull      : hand -> int
+    val getCaree     : hand -> int
+    val getFlushStr8 : hand -> int
 
     val kicker    : hand -> hand -> int -> unit 
     val arbitThem : hand -> int -> unit 
