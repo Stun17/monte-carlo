65c8d93ad58d2e60ef16bdfc275813a8784805ed
Your Name <you@example.com>
2019-11-18 22:48:23 +0000

    draft for all streets

--- TH/decisions.ml
+++ TH/decisions.ml
@@ -11,18 +11,16 @@ struct
 
   (* is there three or more cards in suit *)
   let isColor xs =
-    if (5 > List.length xs)
-    then false
-    else (List.map (countSuit xs) [0;1;2;3] |> List.filter (fun x -> x > 2) |> 
-            List.length |> fun x -> x > 0
+    (4 < List.length xs) &&
+      (List.map (countSuit xs) [0;1;2;3] |>
+         List.filter (fun x -> x > 2) |>
+         List.length |> fun x -> x > 0
       )
   ;;
 
   (* is there pair *)
   let isWet xs =
-    if (3 > List.length xs)
-    then false
-    else
+    (3 < List.length xs) &&
     (List.map (countRank xs) [0;1;2;3;4;5;6;7;8;9;10;11;12] |>
        List.filter (fun x -> x > 1) |> List.length |> fun x -> x > 0
     )
@@ -36,79 +34,57 @@ struct
   ;;
 
   let isDupal cs =
-    if (4 > List.length cs)
-    then false
-    else
+    (3 < List.length cs) &&
     (List.map (countRank cs) [0;1;2;3;4;5;6;7;8;9;10;11;12] |>
        List.filter (fun x -> x == 2) |> List.length |> fun x -> x == 2)
   ;;
     
   let isSet cs =
-    if (3 > List.length cs)
-    then false
-    else (List.map (countRank cs) [0;1;2;3;4;5;6;7;8;9;10;11;12] |> List.exists (fun x -> x == 3))
+    (2 < List.length cs) &&
+    (List.map (countRank cs) [0;1;2;3;4;5;6;7;8;9;10;11;12] |> List.exists (fun x -> x == 3))
   ;;
 
   let isStraight cs =
-    if (5 > List.length cs)
-    then false
-    else
+    (4 < List.length cs) &&
     (List.split cs |> fst |> List.sort compare |>
        fun xs ->
        match xs with
        | [m0;m1;m2;m3;m4;m5;m6] ->
-             ( m4 - m0 == 4 && m4 - m1 == 3 && m4 - m2 == 2 && m4 - m3 == 1)
-          || ( m5 - m1 == 4 && m5 - m2 == 3 && m5 - m3 == 2 && m5 - m4 == 1)
-          || ( m6 - m2 == 4 && m6 - m3 == 3 && m6 - m4 == 2 && m6 - m5 == 1)
-          || ( m6 == 12 && m3 == 3  && m2 == 2  && m1 == 1  && m0 == 0)
+             ( m4 - m0 == 4 && m4 - m1 == 3 && m4 - m2 == 2 && m4 - m3 == 1 )
+          || ( m5 - m1 == 4 && m5 - m2 == 3 && m5 - m3 == 2 && m5 - m4 == 1 )
+          || ( m6 - m2 == 4 && m6 - m3 == 3 && m6 - m4 == 2 && m6 - m5 == 1 )
+          || ( m6 == 12 && m3 == 3  && m2 == 2  && m1 == 1  && m0 == 0 )
        | [m0;m1;m2;m3;m4;m5] ->
-             ( m5 - m1 == 4 && m5 - m2 == 3 && m5 - m3 == 2 && m5 - m4 == 1)
+             ( m5 - m1 == 4 && m5 - m2 == 3 && m5 - m3 == 2 && m5 - m4 == 1 )
           || ( m4 - m0 == 4 )
+       | [m0;m1;m2;m3;m4] ->
+             ( m4 - m0 == 4 )           
        | _ -> false
     )
   ;;
 
   let isFlush cs =
-    if (5 > List.length cs)
-    then false
-    else (List.map (countSuit cs) [0;1;2;3] |> List.exists (fun x -> x >= 5))
+    (5 > List.length cs) && (List.map (countSuit cs) [0;1;2;3] |> List.exists (fun x -> x > 4))
   ;;
       
-  let isFull cs =
+  let isFull cs = 
     if (5 > List.length cs)
     then false
-    else ((isSet cs) && (isPair cs))
+    else isSet cs && isPair cs
   ;;
     
-  let isCaree cs =
+  let isCaree cs = 
     if (4 > List.length cs)
     then false
     else (List.map (countRank cs) [0;1;2;3;4;5;6;7;8;9;10;11;12] |> List.exists (fun x -> x == 4))
   ;;
 
-  let isFlushStr8 cs =
-    if (5 > List.length cs)
-    then false
-    else
-    (let proc n =
-       List.filter (fun (_, s) -> s == n) cs |> List.split |> fst |> 
-         List.sort compare |> 
-         fun zs ->
-         match zs with
-         | [m0;m1;m2;m3;m4] ->             (m4 - m0 == 4)
-                                        || (m4 == 12 && m0 == 0 && m3 == 3)
-         | [m0;m1;m2;m3;m4;m5] ->          (m4 - m0 == 4)
-                                        || (m5 - m1 == 4)
-                                        || (m5 == 12 && m0 == 0 && m3 == 3)
-         | [m0;m1;m2;m3;m4;m5;m6] ->       (m4 - m0 == 4)
-                                        || (m5 - m1 == 4)
-                                        || (m6 - m2 == 4)
-                                        || (m6 == 12 && m0 == 0 && m3 == 3)
-         | _ -> false
-     in if (isFlush cs) && (isStraight cs)
-        then (List.map proc [0;1;2;3] |> List.exists (fun x -> x == true))
-        else false
-    )
-    ;;
+  let isFlushStr8 cs = 
+      if (isFlush cs && isStraight cs)
+      then 
+        let p n = (List.filter (fun (_, s) -> s == n) cs) |> isStraight
+        in (List.map p [0;1;2;3]) |> List.exists (fun x -> x == true)
+      else false
+  ;;
     
 end ;;
--- TH/evaluations.ml
+++ TH/evaluations.ml
@@ -4,54 +4,48 @@ module Evaluations =
   struct
 
     type hand = (int * int) list (* list of pairs rank/suit *)
-    
-    let priceHigh (xs : hand) =
-      List.sort compare xs |> List.rev |> List.split |> fst |> List.hd
-    ;;
+
+    let takeRanks xs = List.sort compare xs |> List.rev |> List.split |> fst
+              
+    let priceHigh (xs : hand) = takeRanks xs |> List.hd ;;
 
     let pricePair (xs : hand) =
-      let n = List.sort compare xs |> List.rev |> List.split |> fst |>
-                fun ys ->
-                List.fold_right
-                  (fun x acc -> if x == acc then acc else x)
-                  (List.tl ys)
-                  (List.hd ys)
-      in let k1 = List.filter (fun (r,_) -> r != n) xs |> priceHigh
-         in let k2 = List.filter (fun (r,_) -> r != n && r != k1) xs |> priceHigh
-            in let k3 = List.filter (fun (r,_) -> r != n && r != k1 && r != k2) xs |> priceHigh
-               in 3000 * (n + 1) + 200 * (k1 + 1) + 13 * (k2 + 1) + k3
+      let k0 = takeRanks xs |> fun ys -> List.fold_right (fun x acc -> if x == acc then acc else x)
+                                                         (List.tl ys)
+                                                         (List.hd ys)
+      in let k1 = List.filter (fun (r,_) -> r != k0) xs |> priceHigh
+         in let k2 = List.filter (fun (r,_) -> r != k0 && r != k1) xs |> priceHigh
+            in let k3 = List.filter (fun (r,_) -> r != k0 && r != k1 && r != k2) xs |> priceHigh
+               in 3000 * (k0 + 1) + 200 * (k1 + 1) + 13 * (k2 + 1) + k3
     ;;
 
     let priceDupal (xs : hand) =
-      let (n1, n2) =
-        List.sort compare xs |> List.rev |> List.split |> fst |>
-          fun ys ->
-          let ys1 = ys @ [0]
-          and ys2 = 0 :: ys
-          in List.map2 (fun s1 s2 -> if s1 = s2 then s1 else 0) ys1 ys2 |>
-               List.sort compare |> List.rev |>
-               fun zs -> List.hd zs , List.hd (List.tl zs)
+      let (n1, n2) = takeRanks xs |> fun ys ->
+                                     let ys1 = ys @ [0]
+                                     and ys2 = 0 :: ys
+                                     in List.map2 (fun s1 s2 -> if s1 = s2 then s1 else 0) ys1 ys2 |>
+                                          List.sort compare |> List.rev |>
+                                          fun zs -> List.hd zs , List.hd (List.tl zs)
       in let k = List.filter (fun (r,_) -> r != n1 && r != n2) xs |> priceHigh 
-         in 3000 * (n1 + 1) + 14 * (n2 + 1) + k
+         in 300 * (n1 + 1) + 14 * (n2 + 1) + k
     ;;
 
     let priceSet (xs : hand) =
-      let n = List.sort compare xs |> List.rev |> List.split |> fst |>
-                fun ys ->
-                List.map2 (fun s1 s2 -> if s1 = s2 then s1 else 0) (ys @ [0]) (0 :: ys) |>
-                  List.sort compare |> List.rev |> List.hd
+      let n = takeRanks xs |> fun ys ->
+                              List.map2 (fun s1 s2 -> if s1 = s2 then s1 else 0)
+                                        (ys @ [0])
+                                        (0 :: ys) |>
+                                List.sort compare |> List.rev |> List.hd
       in let k1 = List.filter (fun (r, _) -> r != n) xs |> priceHigh
          in let k2 = List.filter (fun (r, _) -> r != n && r != k1) xs |> priceHigh
-            in 3000 * (n + 1) + 14 * (k1 + 1) + k2
+            in 300 * (n + 1) + 14 * (k1 + 1) + k2
     ;;
 
     let priceStr8 (xs : hand) =
-      List.sort compare xs |> List.rev |> List.split |> fst |>
+      takeRanks xs |>
         fun ys ->
-        let ys1 = ys @ [0]
-        and ys2 = 0 :: ys
-        in List.map2 (fun s1 s2 -> if s2 - s1 = 1 then s2 else 0) ys1 ys2 |>
-             List.sort compare |> List.rev |> List.hd  
+        List.map2 (fun s1 s2 -> if s2 - s1 = 1 then s2 else 0) (ys @ [0]) (0 :: ys) |>
+          List.sort compare |> List.rev |> List.hd  
 
     let priceFlush (xs : hand) =
       let f n  = List.filter (fun (_, s) -> s == n) xs
@@ -82,10 +76,6 @@ module Evaluations =
             in 200 * (n + 1) + k
     ;;
 
-    let priceFlushStr8 (xs : hand) =
-      let fd = priceFlush xs
-      and sd = priceStr8  xs
-      in min fd sd
-    ;;
+    let priceFlushStr8 (xs : hand) = min (priceFlush xs) (priceStr8 xs) ;;
      
   end ;;  
