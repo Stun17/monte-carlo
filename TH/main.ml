ba936bebe7ee0a9638c56c707ac20df7ba3e541a
Your Name <you@example.com>
2019-11-25 09:39:52 +0000

    with input check in main now

--- TH/main.ml
+++ TH/main.ml
@@ -1,23 +1,34 @@
-(* texas holdem poker game statistics calculator
+(* texas holdem poker game pocket cards value statistics calculator
+
     input params :      number of hands
                         number of gamers
     ouput        :      win-poket(rank/suit)
- *)
+*)
 
-open Shuffle ;;  open Treatment ;; open Dealing ;; open Arbitrage ;; open Bat ;;
+open Shuffle ;;
+open Treatment ;;
+open Dealing ;;
+open Arbitrage ;;
+open Bat ;;
 
+exception BadParams ;;
+
+if 3 != Array.length Sys.argv
+then (print_endline "usage: a.out #numOfHands (> 0) #numOfPlayers (2 .. 10)" ; raise BadParams)
+else () ;;
+    
 let numOfHands =
-  int_of_string (Sys.argv.(1)) ;; (* get number of hands fm command line params   *)
+  int_of_string (Sys.argv.(1)) ;; (* get number of hands fm script command line params   *)
 
 if numOfHands < 0 
-then (print_endline "usage: a.out #numOfHands (> 0) #numOfPlayers (2 .. 10)" ; exit 1)
+then (print_endline "usage: a.out #numOfHands (> 0) #numOfPlayers (2 .. 10)" ; raise BadParams )
 else () ;;
 
 let numOfGamers = 
-  int_of_string (Sys.argv.(2)) ;; (* get number of players fm command line params *)
+  int_of_string (Sys.argv.(2)) ;; (* get number of players fm script command line params *)
   
 if numOfGamers < 2 || numOfGamers > 10  
-then (print_endline "usage: a.out #numOfHands (> 0) #numOfPlayers (2 .. 10)" ; exit 2 )
+then (print_endline "usage: a.out #numOfHands (> 0) #numOfPlayers (2 .. 10)" ; raise BadParams )
 else () ;;
 
 Treatment.inithash () ;; (* prepare hash-table for data *)
